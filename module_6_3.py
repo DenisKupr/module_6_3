0cf400596ef281b160913576f346364ae3fbeb99
Denis <zaq.den@gmail.com>
2024-07-19 21:11:10 +0700

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -16,25 +16,27 @@ class Eagle:
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        Horse.__init__(self)
-        Eagle.__init__(self)
+        Horse.__init__(self)  # Инициализация родительского класса Horse
+        Eagle.__init__(self)  # Инициализация родительского класса Eagle
 
     def move(self, dx, dy):
-        self.run(dx)
-        self.fly(dy)
+        self.run(dx)  # Запускаем метод run из класса Horse
+        self.fly(dy)  # Запускаем метод fly из класса Eagle
 
     def get_pos(self):
-        return (self.x_distance, self.y_distance)
+        return (self.x_distance, self.y_distance)  # Возвращаем текущее положение
 
     def voice(self):
-        print(self.sound)
+        print(self.sound)  # Печатаем звук, который издает орел, используя экземпляр класса
 
+# Создаем объект класса Pegasus
 p1 = Pegasus()
 
-print(p1.get_pos())
+# Примеры работы с объектом
+print(p1.get_pos())  # Вывод: (0, 0)
 p1.move(10, 15)
-print(p1.get_pos())
+print(p1.get_pos())  # Вывод: (10, 15)
 p1.move(-5, 20)
-print(p1.get_pos())
+print(p1.get_pos())  # Вывод: (5, 35)
 
 p1.voice()
